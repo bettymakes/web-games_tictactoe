6ff3027703ad3f9cdedcac12c7e701f1697174e8
blibotics <blibotics@gmail.com>
2013-06-19 00:43:34 -0400

    Started to build game logic in .js file

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
+	// $('.square').mouseenter(function(){
+	// 	$(this).css({
+	// 		'background': 'blue',
+	// 		'cursor': 'pointer'
+	// 	});
+	// });
+	// $('.square').mouseleave(function(){
+	// 	$(this).css({
+	// 		'background': 'black',
+	// 	});
+	// });
+	counter = 0;
+
+	$(".square").on("mouseenter",function() {
+		$(this).addClass("hover_square");
+	});
+	$(".square").on("mouseleave",function() {
+		$(this).removeClass("hover_square");
+	});
+
+	$(".square").on("click", function() {
+		counter++;
+
+	if (counter % 2 == 1) {
+		$(this).text("X");
+	}
+	
+	else {
+		$(this).text("O");
+	}
+
+
+
+	});
 
 });
